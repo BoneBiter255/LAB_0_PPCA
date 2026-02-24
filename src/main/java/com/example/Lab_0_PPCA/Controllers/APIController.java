84998109e126a4ac73986a0bfb5633aa7b0b887a
BoneBiter255 <felix2295772@gmail.com>
2026-02-24 15:46:06 -0500

    Docker settings agregado

--- src/main/java/com/example/Lab_0_PPCA/Controllers/APIController.java
+++ src/main/java/com/example/Lab_0_PPCA/Controllers/APIController.java
@@ -3,7 +3,6 @@ package com.example.Lab_0_PPCA.Controllers;
 import com.example.Lab_0_PPCA.Services.APIService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
